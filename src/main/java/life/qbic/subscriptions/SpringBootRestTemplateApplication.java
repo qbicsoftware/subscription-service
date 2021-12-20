a9420e5a2aa4fb0afd7497efd3ce098f514aa6e6
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2021-12-20 10:27:56 +0100

    Provide example logger instance

--- src/main/java/life/qbic/subscriptions/SpringBootRestTemplateApplication.java
+++ src/main/java/life/qbic/subscriptions/SpringBootRestTemplateApplication.java
@@ -1,13 +1,17 @@
 package life.qbic.subscriptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootRestTemplateApplication {
 
+  private static final Logger logger = LoggerFactory.getLogger(SpringBootRestTemplateApplication.class);
+
   public static void main(String[] args) {
+    logger.info("Starting subscription service ...");
     SpringApplication.run(SpringBootRestTemplateApplication.class, args);
   }
 
